1622380386beb41e34ed292316b9e4fe57683d69
DesireeMcElroy <desireeashley90@gmail.com>
2026-04-23 10:05:02 -0500

    updates

---
+++ D212-Data_Mining_II/task_1/preprocess.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+
+filepath = 'medical_clean.csv'
+
+
+continuous_columns = [
+    'population',
+    'children',
+    'age',
+    'income',
+    'vitd_levels',
+    'doc_visits',
+    'full_meals_eaten',
+    'length_of_stay',
+    'total_charges',
+    'additional_charges'
+]
+
+
+
+def preprocess_df(df=None, keep_continuous=True):
+    '''
+    Preprocessing dataframe for clustering task:
+    - read in csv if df not provided
+    - lowercase column names
+    - rename columns for readability
+    - set case_order as index
+    '''
+
+    ## read in df if not provdided
+    if df is None:
+        df = pd.read_csv(filepath)
+
+    df.columns = df.columns.str.lower()
+    print(f'original dataframe shape: {df.shape}')
+
+    df.rename(columns={'totalcharge': 'total_charges',
+                       'initial_days': 'length_of_stay',
+                       'caseorder': 'case_order'}, inplace=True)
+
+    df.set_index('case_order', inplace=True)
+    if keep_continuous:
+        df = df[continuous_columns]
+
+    print(f'new dataframe shape: {df.shape}')
+
+    return df
+
+
+
